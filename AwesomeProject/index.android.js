08a55ced49b0cec018d79a9d70522fcb89099bdf
Bri Dotson <bridotson@gmail.com>
2016-11-12 07:48:56 -0800

    change styling and add initial state

--- AwesomeProject/index.android.js
+++ AwesomeProject/index.android.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import { AppRegistry, Image, Picker, StyleSheet, Text, TextInput, View } from 'react-native';
 
 var SampleApp = React.createClass({
+  getInitialState: function() {
+    return {
+        kcalPerFeeding: 160,
+      kcalPerKgDryFood: 3825,
+    };
+  },
   componentWillMount: function() {
     this.setState({ showLoading: true});
     
@@ -29,13 +35,15 @@ var SampleApp = React.createClass({
   }, 
   calculateDryFood: function() {
     const amountOfWetFood = this.state.text;
-    const kcalWetFood = this.state.selectedFood/1000*amountOfWetFood;
+    const kcalWetFood = this.state.selectedFood*amountOfWetFood;
     // kgDryFood = (kcalPerFeeding - kcalWetFood) / kcalPerKgDryFood; 
-    const kgDryFood = (160 - kcalWetFood) / 3825;
+    const kgDryFood = (this.state.kcalPerFeeding - kcalWetFood/1000) / this.state.kcalPerKgDryFood;
+    if (isNaN(kgDryFood*1000) === true) {
+        return 0;
+    }
     return Math.round(kgDryFood*1000);
   },
   render: function() {
-    console.log(this.state);
     const pic = {
       uri: 'http://bri616.github.io/fat-cat/gentlemancat.png'
     };
@@ -46,30 +54,42 @@ var SampleApp = React.createClass({
     } else { 
       return (
         <View style={{flex: 1}}>
-          <View style={{flex: 2}}>
-            <Text style={styles.title}>Plutocat</Text>
+          <View style={{flex: 5}}>
+            <Text style={styles.title}>PlutoCat</Text>
             <Image source={pic} style={{flex: 10}} resizeMode={Image.resizeMode.contain}/>
-          </View>
-          <View style={{flex: 2}}>
-            <Picker
-              selectedValue={this.state.selectedFood}
-              onValueChange={
-                (food) => (this.setState({selectedFood: food}))
-              }
-            >
-                {this.state.wetFoods.map((food, i) => {
-                  return <Picker.Item
-                            value={food.kCal}
-                            label={food.title}
-                            key={i}
-                         /> 
-                })}
-            </Picker>
-            <Text>Selected Food kCal per kg: {this.state.selectedFood}</Text>
-            <Text>How much of this food will you give him in grams?</Text>
-            <TextInput onChangeText={(text) => this.setState({text})} />
-            <Text>You should give him {this.calculateDryFood()}g of dry food</Text>
-            
+          </View>        
+          <View style={{flex: 4}}>
+            <View style={{flex: 2, flexDirection: 'row'}}>   
+                <View style={styles.numberBox}>                      
+                <Text style={{flex: 1}}>Wet food in g:</Text>
+                <TextInput style={{flex: 1, textAlign: 'center', fontSize: 20, fontWeight: 'bold'}} onChangeText={(text) => this.setState({text})} keyboardType='numeric'/>
+                    </View>
+                    <View style={styles.numberBox}>
+                        <Text style={{flex: 1}}>Dry food in g:</Text>
+                <TextInput style={{flex: 1, textAlign: 'center', fontSize: 20, fontWeight: 'bold'}} value={this.calculateDryFood().toString()} />
+              </View>
+            </View>
+                        <View style={styles.pickerContainer}>
+              <Picker
+                    itemStyle={{fontSize: 25}}
+                selectedValue={this.state.selectedFood}
+                onValueChange={
+                  (food) => (this.setState({selectedFood: food}))
+                }
+              >
+                  {this.state.wetFoods.map((food, i) => {
+                    return <Picker.Item
+                              value={food.kCal}
+                              label={food.title}
+                              key={i}
+                           /> 
+                  })}
+              </Picker>
+            </View>
+            <View style={styles.footerContainer}>
+                <Text>Image by: xxx from the noun project</Text>
+              <Text>Codes by: bri and ian </Text>
+            </View>
           </View>
         </View>
       );
@@ -80,10 +100,37 @@ var SampleApp = React.createClass({
 const styles = StyleSheet.create({
   title: {
     flex: 3,
-    fontSize: 50,
     fontFamily: 'abrilfatface',
+    fontSize: 50,
     textAlign: 'center',
     color: 'black',
+  },
+  numberBox: {
+    flex: 1,
+    paddingTop:5,
+    paddingBottom:10,
+    paddingLeft:10,
+    paddingRight:10, 
+    borderRadius:10,
+    borderWidth: 3,
+    borderColor: 'black',
+    margin: 3,
+  },
+  pickerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop:5,
+    paddingBottom:10,
+    paddingLeft:10,
+    paddingRight:10, 
+    borderRadius:10,
+    borderWidth: 3,
+    borderColor: 'black',
+    margin: 3,
+  },
+  footerContainer: {
+    flex: 2,
+    justifyContent: 'flex-end',
   },
 });
 
